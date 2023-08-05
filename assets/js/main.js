edddb6e3ebb22efddea31cb701b8ed654206b5fe
Bruno Reis <bruno.reiis15@gmail.com>
2023-08-05 18:14:04 -0300

    Mudando texto e colocando links na Nav

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const PremiumOff = () => {
+  alert(
+    "Para utilizar o premium fale com o meu criador no serividor de discord NexusFt"
+  );
+};
+
+const Nexus = () => {
+  window.open("https://discord.gg/J2VNMcPakP", "_blank");
+};
+
+const Adicionar = () => {
+  window.open(
+    "https://discord.com/api/oauth2/authorize?client_id=1040416053571899494&scope=applications.commands+bot&permissions=8",
+    "_blank"
+  );
+};
+
+const gitBruno = () => {
+    window.open("https://github.com/BrunoReiis", "_blank");
+}
